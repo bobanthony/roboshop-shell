e832304968d8b23f65b65eb30a83619df26777df
Dinesh <satyadineshk328@gmail.com>
2023-02-28 17:49:00 +0530

    updating ./roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,14 @@
+
+source common.sh
+
+
+
+print_head "set up mongodb repos"
 cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
+print_head "installing mongodb"
 yum install mongodb-org -y
+print_head "enabling mongodb"
 systemctl enable mongod
+print_head "starting mongodb"
 systemctl start mongod
 # update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
