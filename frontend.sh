9652bbfcf8f422e7561bb9739b496be57505d3f3
Dinesh <satyadineshk328@gmail.com>
2023-02-28 13:41:02 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -2,20 +2,23 @@
 code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm-f ${log_file} 2>/tmp/roboshop.log
-echo -e "\e[36minstalling nginx\e[0m"
+print_head() (
+echo -e "\e[36m$1\e[0m"
+)
+print_head "installing nginx"
 yum install nginx -y &>>${log_file}
-echo -e "\e[36enable nginx\e[0m"
+print_head "enable nginx"
 systemctl enable nginx  &>>${log_file}
-echo -e "\e[36mstarting nginx\e[0m"
+print_head "starting nginx"
 systemctl start nginx  &>>${log_file}
-echo -e "\e[36mremoving contents frontend\e[0m"
+print_head "removing contents frontend"
 rm -rf /usr/share/nginx/html/*  &>>${log_file}
-echo -e "\e[36mdownloading and installing  frontend\e[0m"
+print_head "downloading and installing  frontend"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file}
-echo -e "\e[36mextracting downloaded frontend\e[0m"
+print_head "extracting downloaded frontend"
 cd /usr/share/nginx/html  &>>${log_file}
 unzip /tmp/frontend.zip  &>>${log_file}
-echo -e "\e[36mcopying nginx configs\e[0m"
+print_head "copying nginx configs"
 cp ${code_dir}/configs/nginx.roboshop.configs /etc/nginx/default.d/roboshop.conf
-echo -e "\e[36mrestarting nginx\e[0m"
+print_head "restarting nginx"
 systemctl restart nginx  &>>${log_file}
