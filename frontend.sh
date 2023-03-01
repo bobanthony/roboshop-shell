42c5bec860655898e20fa38d6611565d9e159ac0
Dinesh <satyadineshk328@gmail.com>
2023-03-01 11:44:41 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -2,20 +2,21 @@
 source common.sh
 print_head "installing nginx"
 # shellcheck disable=SC2154
-yum install nginx -y &>>${log_file}
+yum install nginx -y &>>"${log_file}"
 print_head "enable nginx"
-systemctl enable nginx  &>>${log_file}
+systemctl enable nginx  &>>"${log_file}"
 print_head "starting nginx"
-systemctl start nginx  &>>${log_file}
+systemctl start nginx  &>>"${log_file}"
 print_head "removing contents frontend"
-rm -rf /usr/share/nginx/html/*  &>>${log_file}
+rm -rf /usr/share/nginx/html/*  &>>"${log_file}"
 print_head "downloading and installing  frontend"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>"${log_file}"
 print_head "extracting downloaded frontend"
+# shellcheck disable=SC2164
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip  &>>${log_file}
+unzip /tmp/frontend.zip  &>>"${log_file}"
 print_head "copying nginx configs"
 # shellcheck disable=SC2154
-cp "${code_dir}/configs/nginx.roboshop.configs" /etc/nginx/default.d/roboshop.conf &>>${log_file}
+cp "${code_dir}/configs/nginx.roboshop.configs" /etc/nginx/default.d/roboshop.conf &>>"${log_file}"
 print_head "restarting nginx"
-systemctl restart nginx  &>>${log_file}
+systemctl restart nginx  &>>"${log_file}"
