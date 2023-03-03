6ed119563ee3cd1f66abbbaa9958cb7981a2c438
Dinesh <satyadineshk328@gmail.com>
2023-03-03 13:17:13 +0530

    updating ./roboshop-shell

---
+++ if.sh
@@ -0,0 +1,9 @@
+# if command deals with both numbers and strings
+if [ $name == dinesh ]; then
+  echo my name is satya dinesh
+  elif [ $name == bob ]; then
+    echo my name is bob anthony
+    else  No Name available
+      
+
+fi
